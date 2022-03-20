78f6bb9f53cdd650aca2cc00409a8fb6a53cff06
nogayama <NOGAYAMA@jp.ibm.com>
2022-03-20 12:56:21 +0900

    feat: script and dependencies
    Signed-off-by: nogayama <NOGAYAMA@jp.ibm.com>

---
+++ src/dw/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# =================================================================
+# dw
+#
+# Copyright (c) 2022 Takahide Nogayama
+#
+# This software is released under the MIT License.
+# http://opensource.org/licenses/mit-license.php
+# =================================================================
+
+def add_one(number):
+    return number + 1
