26d370c8e75a9de9ce9170a81a06e6564d571399
nogayama <NOGAYAMA@jp.ibm.com>
2022-04-03 01:11:05 +0900

    feat: dw bytes grep supports ignore case
    Signed-off-by: nogayama <NOGAYAMA@jp.ibm.com>

--- src/dw/bytes/grep.py
+++ src/dw/bytes/grep.py
@@ -26,20 +26,24 @@ _LOGGER: logging.Logger = logging.getLogger(__name__)
 
 ###################################################################
 @unit_func_constructor
-def grep(input_file=None, patterns=None):
+def grep(input_file=None, patterns=None, max_count=None, ignore_case=False):
     if not input_file: input_file = "-"
     if not patterns: patterns = []
     for pattern in patterns:
         if not isinstance(pattern, bytes):
             raise ValueError("pattern must be bytes object")
+    regexp_flag: int=0
+    if ignore_case:
+        regexp_flag = regexp_flag | re.IGNORECASE
     
     @transform_func(input_file)
     def func(input_iterable):
         # Main
         if patterns:
-            regex_patterns = [re.compile(pattern) for pattern in patterns if pattern]
+            regex_patterns = [re.compile(pattern, flags=regexp_flag) for pattern in patterns if pattern]
             input_iterable_bk = input_iterable
             def ite():
+                count = 0
                 for idx, b in enumerate(input_iterable_bk):
                     print_this_line = False
                     for regex_pattern in regex_patterns:
@@ -47,20 +51,23 @@ def grep(input_file=None, patterns=None):
                             print_this_line = True
                             break
                     if print_this_line:
+                        count += 1
                         yield b
+                    if max_count and count >= max_count:
+                        break
             return ite()
         return input_iterable
     return func
 
 ###################################################################
 def main_str(input_file: str=None, output_file: str=None,
-            patterns=None,
+            patterns=None, max_count=None, ignore_case=False,
             ) -> int:
     if not input_file: input_file = "-"
     if not output_file: output_file = "-"
     if not patterns: patterns = []
 
-    grep(input_file, patterns) > dw.bytes.to_file(output_file)
+    grep(input_file, patterns, max_count, ignore_case) > dw.bytes.to_file(output_file)
         
     return 0
 
@@ -74,13 +81,28 @@ def add_patterns_args():
         return dw.cli.ArgparseMonad(argparse_wrapper)
     return func
 
+def add_max_count_args():
+    def func(argparse_wrapper):
+        argparse_wrapper.arg_parser.add_argument('-m', '--max-count', dest="max_count", metavar="NUM", nargs=None, default=None, type=int,
+                                                 help="stop after NUM selected lines")
+        return dw.cli.ArgparseMonad(argparse_wrapper)
+    return func
+
+def add_ignore_case_args():
+    def func(argparse_wrapper):
+        argparse_wrapper.arg_parser.add_argument('-i', '--ignore-case', dest="ignore_case", default=False, action='store_true',
+                                                 help="ignore case distinctions in patterns and data")
+        return dw.cli.ArgparseMonad(argparse_wrapper)
+    return func
 
 CLI: dw.cli.ArgparseMonad = dw.cli.argparse_monad("grep", "print lines that match patterns", aliases=["filter"], sub_command_of=DW_BYTES_CLI, main_func=main_str) \
                                 | dw.cli.add_version_arg(dw.__version__) \
                                 | dw.cli.add_log_args() \
                                 | dw.cli.add_input_file_args() \
                                 | dw.cli.add_output_file_args() \
-                                | add_patterns_args()
+                                | add_patterns_args() \
+                                | add_max_count_args() \
+                                | add_ignore_case_args()
 
 if __name__ == "__main__":
     import sys
