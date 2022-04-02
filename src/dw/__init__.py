4e91faad753c971f970c046873b8ab3919738df3
nogayama <NOGAYAMA@jp.ibm.com>
2022-04-03 00:28:18 +0900

    fix: not change log level of dw
    Signed-off-by: nogayama <NOGAYAMA@jp.ibm.com>

--- src/dw/__init__.py
+++ src/dw/__init__.py
@@ -12,7 +12,7 @@
 # https://future-architect.github.io/articles/20201223/
 from __future__ import annotations
 
-__version__="0.0.17"
+__version__="0.0.18"
 
 from collections.abc import Iterable, Mapping, Callable
 import logging
@@ -21,13 +21,12 @@ import sys
 
 import yaml
 
-DEFAULT_LOG_FORMAT: str = os.environ.get('DW_LOG_FORMAT', '%(asctime)s | %(threadName)-10s | %(levelname)-5s | %(name)s | %(message)s (%(filename)s L%(lineno)s)')
+DEFAULT_LOG_FORMAT: str = os.environ.get('DW_LOG_FORMAT', '%(asctime)s | %(threadName)-10s | %(levelname)-7s | %(name)s | %(message)s (%(filename)s L%(lineno)s)')
 DEFAULT_LOG_LEVEL: str = os.environ.get('DW_LOG_LEVEL', 'WARNING')
 DEFAULT_LOG_OUTPUT: str = os.environ.get('DW_LOG_OUTPUT', 'stdout')
 
 # Logger
 _LOGGER: logging.Logger = logging.getLogger(__name__)
-_LOGGER.setLevel(DEFAULT_LOG_LEVEL)
 
 ###################################################################
 
